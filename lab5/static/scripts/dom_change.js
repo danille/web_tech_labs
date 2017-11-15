cc10b0f8accae3b477e0cce16c08b6a59d682473
Danylo Lebediev <danilleock@gmail.com>
2017-11-15 13:45:16 +0100

    A6: Added dynamic font and bg color changing

--- lab5/static/scripts/dom_change.js
+++ lab5/static/scripts/dom_change.js
@@ -17,10 +17,32 @@ window.onload = function () {
         }
     }
 
+    function changeHeadersFont() {
+        var fontFamilyList = document.getElementById('id_font_list');
+        var fontFamilyName = fontFamilyList.options[fontFamilyList.selectedIndex].value;
+
+        var headers = document.getElementsByTagName('h4');
+
+        console.log(headers);
+
+        for (var i = 0; i < headers.length; i++){
+            headers[i].style.fontFamily = fontFamilyName;
+        }
+    }
+
+    function changeBackgroundColor() {
+        var bgColorInput = document.getElementById('id_bg_color_input');
+        document.body.style.backgroundColor = bgColorInput.value;
+    }
+
     var resetButton = document.getElementById("id_bt_add_headers");
     var hideLinksButton = document.getElementById('id_bt_hide_links');
+    var changeHeadersFontButton = document.getElementById('id_bt_change_headers_font');
+    var changeBgColorButton = document.getElementById('id_bt_change_bg_color');
 
     resetButton.addEventListener('click', addMainHeader, false);
     hideLinksButton.addEventListener('click', hideLinks, false);
+    changeHeadersFontButton.addEventListener('click', changeHeadersFont, false);
+    changeBgColorButton.addEventListener('click', changeBackgroundColor, false);
 };
 
