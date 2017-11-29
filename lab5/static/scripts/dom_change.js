1c3998ebb242e2fe45a118dcb25e263ed6b85b35
Danylo Lebediev <danilleock@gmail.com>
2017-11-29 11:53:26 +0100

    A6: Finished Assignment 6

--- lab5/static/scripts/dom_change.js
+++ lab5/static/scripts/dom_change.js
@@ -2,11 +2,11 @@ window.onload = function () {
 
     function addMainHeader() {
         var headerText = "Lorem Ipsum Times";
-        var mainHeaderText = document.createElement('h1');
+        var mainHeaderText = document.createElement("h1");
         var content = document.createTextNode(headerText);
         mainHeaderText.appendChild(content);
 
-        var mainHeader = document.getElementById('id_main_header');
+        var mainHeader = document.getElementById("id_main_header");
         mainHeader.appendChild(mainHeaderText);
     }
 
@@ -18,10 +18,10 @@ window.onload = function () {
     }
 
     function changeHeadersFont() {
-        var fontFamilyList = document.getElementById('id_font_list');
+        var fontFamilyList = document.getElementById("id_font_list");
         var fontFamilyName = fontFamilyList.options[fontFamilyList.selectedIndex].value;
 
-        var headers = document.getElementsByTagName('h4');
+        var headers = document.getElementsByTagName("h4");
 
         console.log(headers);
 
@@ -41,22 +41,22 @@ window.onload = function () {
     }
 
     function changeBackgroundColor() {
-        var bgColorInput = document.getElementById('id_bg_color_input');
+        var bgColorInput = document.getElementById("id_bg_color_input");
         document.body.style.backgroundColor = bgColorInput.value;
     }
 
     var resetButton = document.getElementById("id_bt_add_headers");
-    var hideLinksButton = document.getElementById('id_bt_hide_links');
-    var changeHeadersFontButton = document.getElementById('id_bt_change_headers_font');
-    var changeBgColorButton = document.getElementById('id_bt_change_bg_color');
-    var getFocusButton = document.getElementById('id_bt_get_focus');
-    var looseFocusButton = document.getElementById('id_bt_loose_focus');
+    var hideLinksButton = document.getElementById("id_bt_hide_links");
+    var changeHeadersFontButton = document.getElementById("id_bt_change_headers_font");
+    var changeBgColorButton = document.getElementById("id_bt_change_bg_color");
+    var getFocusButton = document.getElementById("id_bt_get_focus");
+    var looseFocusButton = document.getElementById("id_bt_loose_focus");
 
-    resetButton.addEventListener('click', addMainHeader, false);
-    hideLinksButton.addEventListener('click', hideLinks, false);
-    changeHeadersFontButton.addEventListener('click', changeHeadersFont, false);
-    changeBgColorButton.addEventListener('click', changeBackgroundColor, false);
-    getFocusButton.addEventListener('click', getFocus, false);
-    looseFocusButton.addEventListener('click', looseFocus, false);
+    resetButton.addEventListener("click", addMainHeader, false);
+    hideLinksButton.addEventListener("click", hideLinks, false);
+    changeHeadersFontButton.addEventListener("click", changeHeadersFont, false);
+    changeBgColorButton.addEventListener("click", changeBackgroundColor, false);
+    getFocusButton.addEventListener("click", getFocus, false);
+    looseFocusButton.addEventListener("click", looseFocus, false);
 };
 
