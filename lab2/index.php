8f1cc014e042136c4193107e25a9dc484843e023
Danylo Lebediev <danilleock@gmail.com>
2017-12-05 16:52:00 +0100

    A8: Add user name and surname processing from cookies.

---
+++ lab2/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>World Times</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+<header>
+    <h3 align="center">
+        <?php
+            if (isset($_COOKIE['user_name']) && (isset($_COOKIE["user_surname"]))) {
+                $user_name = $_COOKIE['user_name'];
+                $user_surname = $_COOKIE["user_surname"];
+                echo "Nice to meet you again ".$user_name." ".$user_surname;
+            } else {
+                echo "It seems to me, we hadn't met before. Don't you want to <a href='register.html'>sign up</a>?";
+            }
+        ?>
+    </h3>
+    <h1 align="center">Lorem Ipsum Times</h1>
+    <nav>
+        <a href="index.php">Main</a>
+        <a href="https://www.lipsum.com">Lorem Ipsum Website</a>
+        <a href="https://www.facebook.com">Facebook</a>
+        <a href="register.html">Register</a>
+    </nav>
+</header>
+
+<aside>
+    <p>This is Lorem Ipsum text, which is used for presentation in typographic.</p>
+</aside>
+
+<article>
+    <header>
+        <h3>Lorem Ipsum?</h3>
+    </header>
+    <section>
+        <h4>What is Lorem Ipsum?</h4>
+        <p>
+            <mark>Lorem Ipsum</mark> is simply dummy text of the printing and typesetting industry. <mark>Lorem Ipsum</mark> has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing <mark>Lorem Ipsum</mark> passages, and more recently with desktop publishing software like Aldus PageMaker including versions of <mark>Lorem Ipsum</mark>.
+        </p>
+    </section>
+    <section>
+        <h4>Why do we use it?</h4>
+        <p>
+            It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+        </p>
+    </section>
+    <section>
+        <h4>Where does it come from?</h4>
+        <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+        </p>
+    </section>
+    <section>
+        <h4>Where can I get some?</h4>
+        <p>
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
+        </p>
+    </section>
+</article>
+
+<footer>
+    <details>
+        <summary>Copyright 45 BC - 2017</summary>
+        <p>Yes, I've put here copyright. 'Cause it's LOREM IPSUM</p>
+        <p>Everybody loves Lorem Ipsum. Grab some Lorem Ipsum for you.</p>
+    </details>
+</footer>
+
+</body>
+</html>
--- lab2/reaction.php
+++ lab2/reaction.php
@@ -1,41 +1,59 @@
+<html>
+    <head>
+        <link type="text/css" rel="stylesheet" href="static/reaction.css">
+    </head>
+    <body>
+    <h3 id="hd-thx">Thank You for Registration!</h3>
+    <h5 id="hd-thx">Please visit our <a href="index.php">main page</a>!</h5>
+
+    </body>
+</html>
 <?php
-echo 'Let\'s see what did you enter'.'<br>';
 
-function print_all_POST_vars() {
-	$post_vars_count = count($_POST);
+$user_name = $_POST["name"];
+$user_surname = $_POST["surname"];
+setcookie("user_name", $user_name, time() + 3600);
+setcookie("user_surname", $user_surname, time() + 3600);
 
-	echo 'It seems to me, that you\'ve entered '. $post_vars_count.' variables'."<br>";
-	echo 'Here they are:'.'<br>';
-	foreach ($_POST as $stuff) {
-    	if (is_array($stuff)) {
-        	foreach ($stuff as $thing) {
-            	echo $thing.'<br>';
-        	}
-    	} else {
-    		if ($stuff != "" && $stuff != 'Submit') {
-    			echo $stuff.'<br>';
-    		}
-	    }
-	}
+function print_all_POST_vars()
+{
+    $post_vars_count = count($_POST);
+
+    echo 'It seems to me, that you\'ve entered ' . $post_vars_count . ' variables' . "<br>";
+    echo 'Here they are:' . '<br>';
+    foreach ($_POST as $stuff) {
+        if (is_array($stuff)) {
+            foreach ($stuff as $thing) {
+                echo $thing . '<br>';
+            }
+        } else {
+            if ($stuff != "" && $stuff != 'Submit') {
+                echo $stuff . '<br>';
+            }
+        }
+    }
 }
 
 
-function validate_telephone_number($telephone_number) {
-	 return preg_match('/^\+48\d{9}/', $telephone_number);
+function validate_telephone_number($telephone_number)
+{
+    return preg_match('/^\+48\d{9}/', $telephone_number);
 }
 
-function check_user_name(){
-	echo 'It seems to me that your name is $user_name. Cool!'.'<br>';
+function check_user_name()
+{
+    echo 'It seems to me that your name is $user_name. Cool!' . '<br>';
 }
 
 
 print_all_POST_vars();
 
-echo "Do you think your telephone number is valid?"."<br>";
+echo "Do you think your telephone number is valid?" . "<br>";
 
 
-if (validate_telephone_number($_POST['tel'])){
-	echo 'It looks like it\'s valid ';
+if (validate_telephone_number($_POST['tel'])) {
+    echo 'It looks like it\'s valid ';
 } else {
-	echo 'Ooops! Your telephone number is not valid!'.'<br>';
+    echo 'Ooops! Your telephone number is not valid!' . '<br>';
 }
+?>
